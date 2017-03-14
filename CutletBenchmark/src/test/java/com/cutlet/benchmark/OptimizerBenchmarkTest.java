97a4a1fc1648450d0e821712dc1ca857f75d4ee5
rudolf muehlbauer <rudolf.muehlbauer@gmail.com>
2017-03-14 19:41:04 +0100

    added testcase

---
+++ CutletBenchmark/src/test/java/com/cutlet/benchmark/OptimizerBenchmarkTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2017 rmuehlba
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.cutlet.benchmark;
+
+import com.cutlet.lib.optimizer.OptimizationStrategy;
+import java.util.logging.Level;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rmuehlba
+ */
+public class OptimizerBenchmarkTest {
+    
+    public OptimizerBenchmarkTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class OptimizerBenchmark.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = new String[]{};
+        OptimizerBenchmark.main(args);
+    }    
+}
